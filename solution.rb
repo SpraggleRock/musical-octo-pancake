c390b6481fdeb11bdd7969bac98a12e0a893fce3
spragglerock <sprasarah@gmail.com>
2016-11-09 20:10:45 -0600

    define WordSequences class

---
+++ solution.rb
@@ -0,0 +1,18 @@
+
+class WordSequences
+
+  def questions
+    "carr give rots rows rrot rrow"
+  end
+
+  def answers
+    "carrots give carrots arrows carrots arrows"
+  end
+
+
+end
+
+
+#### basic tests #####
+puts WordSequences.new.questions
+puts WordSequences.new.answers

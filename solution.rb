e981e49198b10cfade37c0f90239d3a1bf1edcde
spragglerock <sprasarah@gmail.com>
2016-11-09 21:22:02 -0600

    make parse words method to get unrepeated four letter sequences

--- solution.rb
+++ solution.rb
@@ -1,8 +1,20 @@
 
+require 'pry'
+require 'pry-remote'
 class WordSequences
 
   def initialize(words)
     @words = words.split(/\s/)
+  end
+
+  def parse_words
+    @words.reduce([]) do |word_list, word|
+      new_chunks = get_four_letter_sequences(word)
+      repeated_words = word_list & new_chunks
+      new_words = new_chunks - repeated_words
+      old_words = word_list - repeated_words
+      new_words + old_words
+    end
   end
 
   def get_four_letter_sequences(word)
@@ -28,6 +40,8 @@ end
 words = WordSequences.new("arrows carrots give me")
 
 puts words.get_four_letter_sequences("brownie") == ["brow", "rown", "owni", "wnie"]
+
+puts words.parse_words
 
 puts words.questions == "carr give rots rows rrot rrow"
 puts words.answers == "carrots give carrots arrows carrots arrows"

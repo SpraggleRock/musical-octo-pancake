29889e079d21331af05317f87de418823cb58809
spragglerock <sprasarah@gmail.com>
2016-11-10 20:33:40 -0600

    add file timestamp

--- solution.rb
+++ solution.rb
@@ -1,7 +1,8 @@
 require_relative 'lib/word_sequences'
 
-words = WordSequences.new(ARGV[0])
+words = WordSequences.new(ARGV[0] || 'words.txt')
 
-words.questions_file("tmp/questions.txt")
+file_timestamp = Time.now.strftime('%Y%m%d%H%M%s')
+words.questions_file("tmp/questions#{file_timestamp}.txt")
 
-puts words.answers_file("tmp/answers.txt")
+words.answers_file("tmp/answers#{file_timestamp}.txt")

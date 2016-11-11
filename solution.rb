844457eec093b4cf2f1e3b8571224571a3ca5e66
spragglerock <sprasarah@gmail.com>
2016-11-10 20:43:24 -0600

    whitespace

--- solution.rb
+++ solution.rb
@@ -3,6 +3,7 @@ require_relative 'lib/word_sequences'
 words = WordSequences.new(ARGV[0] || 'words.txt')
 
 file_timestamp = Time.now.strftime('%Y%m%d%H%M%s')
+
 words.questions_file("tmp/questions_#{file_timestamp}.txt")
 
 words.answers_file("tmp/answers_#{file_timestamp}.txt")
